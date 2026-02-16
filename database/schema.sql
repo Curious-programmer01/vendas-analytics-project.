48d1fdf52217270dd4e5c27c0974b4d80fa62e61
Curious-programmer01 <julliaferro05@gmail.com>
2026-02-15 22:14:19 -0300

    Create schema.sql

---
+++ database/schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE cliente (
+  id INT PRIMARY KEY,
+  nome VARCHAR(100),
+  email VARCHAR(100),
+  cidade VARCHAR(100)
+  );
+CREATE TABLE produto (
+  id INT PRIMARY KEY,
+  nome VARCHAR(100),
+  preco DECIMAL(10,2)
+  );
+CREATE TABLE pedido (
+  id INT PRIMARY KEY,
+  cliente_id INT,
+  data_pedido DATE,
+  FOREIGN KEY (cliente_id) REFERENCES cliente(id)
+  );
+CREATE TABLE item_pedido (
+  id INT PRIMARY KEY,
+  pedido_id INT,
+  produto_id INT,
+  quantidade INT,
+   FOREIGN KEY (pedido_id) REFERENCES pedido(id),b
+   FOREIGN KEY (produto_id) REFERENCES produto(id)
+  );
