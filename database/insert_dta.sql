305fddd8df38bf09e1370961d96443fcd15f6bef
Curious-programmer01 <julliaferro05@gmail.com>
2026-02-17 19:55:37 -0300

    Update insert_dta.sql

--- database/insert_dta.sql
+++ database/insert_dta.sql
@@ -1,8 +1,8 @@
-INSERT INTO cliente VALUES(1, 'Ana',);
-INSERT INTO cliente VALUES();
-  
-INSERT INTO produto VALUES();
-INSERT INTO produto VALUES();
+INSERT INTO cliente VALUES(1, 'Maria', 'ana@gmail.com', 'São Paulo');
+INSERT INTO cliente VALUES(2, 'João', 'joao@gmail.com', 'Rio de Janeiro');
+
+INSERT INTO produto VALUES(1, 'Notebook', 3500);
+INSERT INTO produto VALUES(2, ');
   
 INSERT INTO pedido VALUES();
 INSERT INTO pedido VALUES();
