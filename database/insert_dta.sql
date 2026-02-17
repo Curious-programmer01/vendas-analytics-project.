f4d123cc9637a2f577c22a50fc9e283193bd4296
Curious-programmer01 <julliaferro05@gmail.com>
2026-02-17 19:37:22 -0300

    Create insert_dta.sql

---
+++ database/insert_dta.sql
@@ -0,0 +1,11 @@
+INSERT INTO cliente VALUES(1, 'Ana',);
+INSERT INTO cliente VALUES();
+  
+INSERT INTO produto VALUES();
+INSERT INTO produto VALUES();
+  
+INSERT INTO pedido VALUES();
+INSERT INTO pedido VALUES();
+
+INSERT INTO item_pedido VALUES();
+INSERT INTO item_pedido VALUES();
